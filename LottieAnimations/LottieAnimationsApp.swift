298e145a313e04d17046c3bbcab0c921e30aa32a
Sergey Shcheglov <97shcheglov@gmail.com>
2022-12-24 13:12:41 +0100

    initial commit

---
+++ LottieAnimations/LottieAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsApp.swift
+//  LottieAnimations
+//
+//  Created by Sergey Shcheglov on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
